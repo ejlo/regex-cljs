bf4ed892b5053d3f8c5086e4fa25e342c7a09e5b
Erik Ouchterlony <erik.ouchterlony@bredband.net>
2012-10-21 10:38:27 +0200

    Changed project.clj for clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject net.cgrand/regex-cljs "1.1.0-SNAPSHOT"
-  :description "a DSL for people who prefer verbose, composable regexes. Clojurescript port"
+(defproject org.clojars.ejlo/regex-cljs "1.1.0-SNAPSHOT"
+  :description "a DSL for people who prefer verbose, composable regexes. Clojurescript port of https://github.com/cgrand/regex"
   :url "http://github.com/ejlo/regex-cljs/"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "[1.4.0,)"]]
   :warn-on-reflection true
 
